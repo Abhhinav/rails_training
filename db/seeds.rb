6570203173e9a63b055aa2365baa66b035be177e
abhhinav <abhinavkmr86@gmail.com>
2020-09-14 14:15:36 +0530

    home with number entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Tag.delete_all
+Post.delete_all
+Category.delete_all
 
 Tag.create(name: "Javascript")
 Tag.create(name: "React")
 Tag.create(name: "Ruby")
 Tag.create(name: "Rails")
+
+cat_web = Category.create(name: "Web", description: "All about Web", published: true)
+cat_js = Category.create(name: "JavaScript", description: "All about JavaScript", published: true)
+cat_prog = Category.create(name: "Programming", description: "All about Programming", published: false)
+
+
+10.times do |i|
+    Post.create(
+        title: "Post #{i}",
+        body: "Body of Post #{i} goes here...",
+        published: true,
+        category_id: i%2 ==0 ? cat_js.id : cat_prog.id
+    )
+end
